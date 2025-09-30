606865959c88978e651093936cf5446be09c91f6
luxian-xinitrc <cusmirlucian@gmail.com>
2025-09-30 22:17:51 +0100

    New commit

--- src/app/page.js
+++ src/app/page.js
@@ -28,12 +28,14 @@ export default function Home() {
   return (
     <section className="h-screen w-screen bg-slate-950 content-center">
     <h1 className="text-5xl text-center font-bold mb-10">Monitoring system</h1>
-      <div className="w-screen flex justify-center">
-        <div className="p-10 w-auto content-center shadow-xl/30 shadow-black rounded-lg">
-          <h1 className="text-2xl font-bold "> <FontAwesomeIcon icon={faThermometerHalf} className="text-green-400"/> {data.temp}</h1>
+      <div className="w-screen gap-2 sm:flex sm:justify-center">
+        <div className="py-10 px-20 w-auto content-center shadow-xl/30 shadow-black rounded-lg">
+        <h1 className="text-2xl font-bold text-center pt-5 pb-5">Temperature</h1>
+          <h1 className="text-2xl font-bold text-center"> <FontAwesomeIcon icon={faThermometerHalf} className="text-green-400"/> {data.temp}</h1>
         </div>
-        <div className="p-10 w-auto content-center shadow-xl/30 shadow-black rounded-lg ml-10">
-          <h1 className="text-2xl font-bold"> <FontAwesomeIcon icon={faTint} className="text-orange-400" /> {data.humi}</h1>
+        <div className="py-10 px-20 w-auto content-center shadow-xl/30 shadow-black rounded-lg sm:ml-10">
+          <h1 className="text-2xl font-bold text-center pt-5 pb-5">Humidity</h1>
+          <h1 className="text-2xl font-bold text-center"> <FontAwesomeIcon icon={faTint} className="text-orange-400" /> {data.humi}</h1>
         </div>
       </div>
     </section>
