5362d045405520a58204cb2de6548e0dd921fe51
lucian-xinitrc <cusmirlucian@gmail.com>
2025-10-24 21:22:45 +0100

    design and api changes

--- src/app/page.js
+++ src/app/page.js
@@ -41,22 +41,22 @@ export default function Home() {
     <div className="sm:flex sm:justify-center">
       <div className="w-screen gap-2 sm:flex sm:justify-center">
         <div className="py-10 px-20 w-auto content-center shadow-xl/30 shadow-black rounded-lg">
-        <h1 className="text-2xl font-bold text-center pt-5 pb-5">Temperature</h1>
-          <h1 className="text-2xl font-bold text-center"> <FontAwesomeIcon icon={faThermometerHalf} className="text-green-400"/> Bubu's {databubu.temp}</h1>
+        <h1 className="text-2xl font-bold text-center pt-5 pb-5">Bubu's Temperature</h1>
+          <h1 className="text-2xl font-bold text-center"> <FontAwesomeIcon icon={faThermometerHalf} className="text-green-400"/> {databubu.temp}</h1>
         </div>
         <div className="py-10 px-20 w-auto content-center shadow-xl/30 shadow-black rounded-lg sm:ml-10">
-          <h1 className="text-2xl font-bold text-center pt-5 pb-5">Humidity</h1>
-          <h1 className="text-2xl font-bold text-center"> <FontAwesomeIcon icon={faTint} className="text-orange-400" /> Bubu's {databubu.humi}</h1>
+          <h1 className="text-2xl font-bold text-center pt-5 pb-5">Bubu's Humidity</h1>
+          <h1 className="text-2xl font-bold text-center"> <FontAwesomeIcon icon={faTint} className="text-orange-400" /> {databubu.humi}</h1>
         </div>
       </div>
       <div className="w-screen gap-2 sm:flex sm:justify-center">
         <div className="py-10 px-20 w-auto content-center shadow-xl/30 shadow-black rounded-lg">
-        <h1 className="text-2xl font-bold text-center pt-5 pb-5">Temperature</h1>
-          <h1 className="text-2xl font-bold text-center"> <FontAwesomeIcon icon={faThermometerHalf} className="text-green-400"/> Dudu's {data.temp}</h1>
+        <h1 className="text-2xl font-bold text-center pt-5 pb-5">Dudu's Temperature</h1>
+          <h1 className="text-2xl font-bold text-center"> <FontAwesomeIcon icon={faThermometerHalf} className="text-green-400"/> {data.temp}</h1>
         </div>
         <div className="py-10 px-20 w-auto content-center shadow-xl/30 shadow-black rounded-lg sm:ml-10">
-          <h1 className="text-2xl font-bold text-center pt-5 pb-5">Humidity</h1>
-          <h1 className="text-2xl font-bold text-center"> <FontAwesomeIcon icon={faTint} className="text-orange-400" /> Dudu's {data.humi}</h1>
+          <h1 className="text-2xl font-bold text-center pt-5 pb-5">Dudu's Humidity</h1>
+          <h1 className="text-2xl font-bold text-center"> <FontAwesomeIcon icon={faTint} className="text-orange-400" /> {data.humi}</h1>
         </div>
       </div>
     </div>
